e260d56c5ce56cd3415169a2b4cae403f6da6ca5
Segi Hovav <shovav@gms4sbc.com>
2020-09-14 10:54:22 -0700

    Extend filter criteria to include Artist, Song name and date created

--- Jellyfin.Plugin.SmartPlaylist/QueryEngine/Operand.cs
+++ Jellyfin.Plugin.SmartPlaylist/QueryEngine/Operand.cs
@@ -5,13 +5,16 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
     public class Operand
     {
         public List<string> Actors { get; set; }
+        public List<string> Artists { get; set; }
         public List<string> Composers { get; set; }
         public float CommunityRating { get; set; }
         public float CriticRating { get; set; }
+        public double DateCreated { get; set; }
         public List<string> Directors { get; set; }
         public List<string> Genres { get; set; }
         public List<string> GuestStars { get; set; }
         public bool IsPlayed { get; set; }
+        public string ItemName { get; set; }
         public string Name { get; set; }
         public string FolderPath { get; set; }
         public double PremiereDate { get; set; }
@@ -21,12 +24,15 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
         public Operand(string name)
         {
             Actors = new List<string>();
+            Artists = new List<string>();
             Composers = new List<string>();
             CommunityRating = 0;
             CriticRating = 0;
+            DateCreated = 0;
             Directors = new List<string>();
             Genres = new List<string>();
             GuestStars = new List<string>();
+            ItemName = "";
             Name = name;
             FolderPath = "";
             PremiereDate = 0;
