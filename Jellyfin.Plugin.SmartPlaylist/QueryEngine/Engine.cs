421044ad5e088208b136ec42b4cb656ebaadb60b
arition <aritionkb@gmail.com>
2022-07-18 22:03:02 -0700

    Update to dotnet 6 / Jellyfin 10.8

--- Jellyfin.Plugin.SmartPlaylist/QueryEngine/Engine.cs
+++ Jellyfin.Plugin.SmartPlaylist/QueryEngine/Engine.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Linq.Expressions;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
@@ -9,13 +9,12 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
     // This is taken entirely from https://stackoverflow.com/questions/6488034/how-to-implement-a-rule-engine
     public class Engine
     {
-        static System.Linq.Expressions.Expression BuildExpr<T>(Expression r, ParameterExpression param)
+        private static System.Linq.Expressions.Expression BuildExpr<T>(Expression r, ParameterExpression param)
         {
-            var left = MemberExpression.Property(param, r.MemberName);
-            var tProp = typeof(T).GetProperty(r.MemberName).PropertyType;
-            ExpressionType tBinary;
+            var left = System.Linq.Expressions.Expression.Property(param, r.MemberName);
+            var tProp = typeof(T).GetProperty(r.MemberName)?.PropertyType;
             // is the operator a known .NET operator?
-            if (ExpressionType.TryParse(r.Operator, out tBinary))
+            if (Enum.TryParse(r.Operator, out ExpressionType tBinary))
             {
                 var right = System.Linq.Expressions.Expression.Constant(Convert.ChangeType(r.TargetValue, tProp));
                 // use a binary operation, e.g. 'Equal' -> 'u.Age == 15'
@@ -25,7 +24,7 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
             if (r.Operator == "MatchRegex" || r.Operator == "NotMatchRegex")
             {
                 var regex = new Regex(r.TargetValue);
-                var method = typeof(Regex).GetMethod("IsMatch", new[] {typeof(string)});
+                var method = typeof(Regex).GetMethod("IsMatch", new[] { typeof(string) });
                 Debug.Assert(method != null, nameof(method) + " != null");
                 var callInstance = System.Linq.Expressions.Expression.Constant(regex);
 
@@ -39,14 +38,15 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
             }
 
             if (tProp.Name == "String")
-            { 
-                var method = tProp.GetMethod(r.Operator, new Type[] { typeof(string) });
+            {
+                var method = tProp.GetMethod(r.Operator, new[] { typeof(string) });
                 var tParam = method.GetParameters()[0].ParameterType;
                 var right = System.Linq.Expressions.Expression.Constant(Convert.ChangeType(r.TargetValue, tParam));
                 // use a method call, e.g. 'Contains' -> 'u.Tags.Contains(some_tag)'
                 return System.Linq.Expressions.Expression.Call(left, method, right);
             }
-            else { 
+            else
+            {
                 var method = tProp.GetMethod(r.Operator);
                 var tParam = method.GetParameters()[0].ParameterType;
                 var right = System.Linq.Expressions.Expression.Constant(Convert.ChangeType(r.TargetValue, tParam));
@@ -58,7 +58,7 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
         public static Func<T, bool> CompileRule<T>(Expression r)
         {
             var paramUser = System.Linq.Expressions.Expression.Parameter(typeof(Operand));
-            System.Linq.Expressions.Expression expr = BuildExpr<T>(r, paramUser);
+            var expr = BuildExpr<T>(r, paramUser);
             // build a lambda function User->bool and compile it
             var value = System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(expr, paramUser).Compile(true);
             return value;
@@ -66,31 +66,27 @@ namespace Jellyfin.Plugin.SmartPlaylist.QueryEngine
 
         public static List<ExpressionSet> FixRuleSets(List<ExpressionSet> rulesets)
         {
-            foreach (var rules in rulesets)
-            {
-                FixRules(rules);
-            }
+            foreach (var rules in rulesets) FixRules(rules);
             return rulesets;
         }
 
         public static ExpressionSet FixRules(ExpressionSet rules)
         {
             foreach (var rule in rules.Expressions)
-            {
                 if (rule.MemberName == "PremiereDate")
                 {
                     var somedate = DateTime.Parse(rule.TargetValue);
                     rule.TargetValue = ConvertToUnixTimestamp(somedate).ToString();
                 }
-            }
+
             return rules;
         }
 
         public static double ConvertToUnixTimestamp(DateTime date)
         {
-            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            TimeSpan diff = date.ToUniversalTime() - origin;
+            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var diff = date.ToUniversalTime() - origin;
             return Math.Floor(diff.TotalSeconds);
         }
-    }	
+    }
 }
